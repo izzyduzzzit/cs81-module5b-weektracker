80615481e16eae59c7442f8eaa61e4d3dfbcbbc8
Izarra Villareal <izarravill@gmail.com>
2025-07-20 18:35:03 -0700

    Added initial myWeek array to activityTracker.js

---
+++ activityTracker.js
@@ -0,0 +1,11 @@
+// Izarra Villareal CS 81 JavaScript Module 5 Assignment 5B: My Week in Data
+
+let myWeek = [
+    { day: "Monday", activity: "Running", category: "Exercise", hoursSpent: 1, enjoyment: 8, timeOfDay: "Morning" },
+    { day: "Tuesday", activity: "Reading", category: "Leisure", hoursSpent: 2, enjoyment: 9, timeOfDay: "Evening" },
+    { day: "Wednesday", activity: "Cycling", category: "Exercise", hoursSpent: 1.5, enjoyment: 7, timeOfDay: "Afternoon" },
+    { day: "Thursday", activity: "Cooking", category: "Chores", hoursSpent: 1, enjoyment: 6, timeOfDay: "Evening" },
+    { day: "Friday", activity: "Yoga", category: "Exercise", hoursSpent: 1, enjoyment: 8, timeOfDay: "Morning" },
+    { day: "Saturday", activity: "Hiking", category: "Exercise", hoursSpent: 3, enjoyment: 10, timeOfDay: "Morning" },
+    { day: "Sunday", activity: "Gaming", category: "Leisure", hoursSpent: 2, enjoyment: 7, timeOfDay: "Afternoon" }
+];
